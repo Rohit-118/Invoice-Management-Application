575db2d4ad51d4eb9c37747e65e3669f0a47f446
Rohit-118 <rohitnayak118@gmail.com>
2023-06-23 01:31:27 +0530

    dynamic

--- src/components/DataFaceDynamic.js
+++ src/components/DataFaceDynamic.js
@@ -1,6 +1,12 @@
 import * as React from 'react';
 import { useEffect } from 'react';
 import { DataGrid } from '@material-ui/data-grid';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import { TablePagination } from '@material-ui/core';
+import { orange } from '@mui/material/colors';
+import Delete from './modal/Delete';
+import Edit from './modal/Edit';
 
 const columns = [
     { field: 'id', headerName: 'SL No', width: 110 },
@@ -15,38 +21,195 @@ const columns = [
     { field: 'amountInUsd', headerName: 'Amount in USD', type: 'number', width: 150 },
 ];
 
+const useStyles = makeStyles((theme) => ({
+    button: {
+        margin: theme.spacing(1),
+        background: orange[500],
+    },
+    tableContainer: {
+        backgroundColor: 'gray',
+        '& .MuiDataGrid-cell': {
+            color: 'white',
+        },
+        '& .MuiDataGrid-columnHeader, .MuiDataGrid-footerContainer': {
+            color: 'white',
+        },
+        '& .MuiDataGrid-checkboxInput.Mui-checked': {
+            color: orange[500],
+        },
+        '& .MuiDataGrid-cellCheckbox .MuiDataGrid-checkboxInput': {
+            color: orange[500],
+        },
+        '& .MuiDataGrid-columnHeaderCheckbox .MuiDataGrid-checkboxInput': {
+            color: orange[500],
+        },
+    },
+}));
+
 export default function DataFaceDynamic() {
-    const [data, setData] = React.useState([]);
     const [pageSize, setPageSize] = React.useState(5);
+    const [page, setPage] = React.useState(0);
+    const [rows, setRows] = React.useState([]);
+    const [deleteOpen, setDeleteOpen] = React.useState(false);
+    const [editOpen, setEditOpen] = React.useState(false);
+    const [selectedColumns, setSelectedColumns] = React.useState([]);
+    const classes = useStyles();
 
     useEffect(() => {
+
         fetch('http://localhost:8081/h2h_milestone_3/ReadServlet')
             .then((response) => response.json())
             .then((json) => {
-                const modifiedData = json.map((row) => ({
+                // Modify the rows array to include 'id' field by incrementing and mapping with 'customer_id'
+                const modifiedData = json.map((row, index) => ({
                     ...row,
-                    id: row.slNo,
+                    id: index + 1, // Incrementing the index to create the 'id' value
                 }));
-                setData(modifiedData);
+                setRows(modifiedData);
             })
             .catch((error) => console.log(error));
     }, []);
 
+    const extractIdToCustomerOrderID = (id) => {
+        const row = rows.find((row) => row.id === id);
+        return row.customerOrderID;
+    };
+
+    const handleRefreshClick = () => {
+        fetch('http://localhost:8081/h2h_milestone_3/ReadServlet')
+            .then((response) => response.json())
+            .then((json) => {
+                // Modify the rows array to include 'id' field by incrementing and mapping with 'customer_id'
+                const modifiedData = json.map((row, index) => ({
+                    ...row,
+                    id: index + 1, // Incrementing the index to create the 'id' value
+                }));
+                setRows(modifiedData);
+            })
+            .catch((error) => console.log(error));
+    };
+
+    const columnNames = {
+        orderCurrency: 'Order Currency',
+        companyCode: 'Company Code',
+        distributionChannel: 'Distribution Channel',
+    };
+    const extractAllColumnData = (id) => {
+        const row = rows.find((row) => row.id === id);
+        return row;
+    };
+    const handleEdit = (updatedValues) => {
+        console.log(extractIdToCustomerOrderID(selectedColumns[0]));
+        console.log(updatedValues);
+        const data = extractAllColumnData(selectedColumns[0]);
+        console.log(data);
+        fetch(`http://localhost:8081/h2h_milestone_3/EditServlet?amountInUsd=${data.amountInUsd}&companyCode=${updatedValues.companyCode}&creditControlArea=${data.creditControlArea}&creditStatus=${data.creditStatus}&customerNumber=${data.customerNumber}&customerOrderID=${data.customerOrderID}&distributionChannel=${updatedValues.distributionChannel}&division=${data.division}&orderAmount=${data.orderAmount}&orderCreationDate=${data.orderCreationDate}&orderCreationTime=${data.orderCreationTime}&orderCurrency=${updatedValues.orderCurrency}&purchaseOrderType=${data.purchaseOrderType}&releasedCreditValue=${data.releasedCreditValue}&requestedDeliveryDate=${data.requestedDeliveryDate}&salesOrg=${data.salesOrg}&slNo=${data.slNo}&soldToParty=${data.soldToParty}&uniqueCustNumber=${data.uniqueCustNumber}`,
+            {
+                mode: 'no-cors',
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            }
+        )
+            .then((response) => {
+                // Check if the request was sent successfully
+                if (response.ok) {
+                    console.log('Request sent successfully');
+                } else {
+                    console.log('Request encountered an error');
+                }
+            }
+            )
+            .catch((error) => console.log(error));
+    };
+    const handleEditClick = () => {
+        setEditOpen(true);
+    };
+    const handleEditClose = () => {
+        setEditOpen(false);
+    };
+
+    const handleDelete = () => {
+        selectedColumns.forEach((columnId) => {
+            console.log(extractIdToCustomerOrderID(columnId));
+            fetch(`http://localhost:8081/h2h_milestone_3/DeleteServlet?customerOrderID=${extractIdToCustomerOrderID(columnId)}`,
+                {
+                    mode: 'no-cors',
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                }
+            )
+                .then((response) => {
+                    // Check if the request was sent successfully
+                    if (response.ok) {
+                        console.log('Request sent successfully');
+                    } else {
+                        console.log('Request encountered an error');
+                    }
+                })
+                .catch((error) => console.log(error));
+        });
+    };
+    const handleDeleteClick = () => {
+        setDeleteOpen(true);
+    };
+    const handleDeleteClose = () => {
+        setDeleteOpen(false);
+    };
+
     return (
         <div style={{ height: 400, width: '100%' }}>
-            <div style={{ display: 'flex', height: '100%' }}>
-                <div style={{ flexGrow: 1 }}>
-                    <DataGrid
-                        rows={data}
-                        columns={columns}
-                        pageSize={pageSize}
-                        rowsPerPageOptions={[5, 10, 20, 50, 100]}
-                        checkboxSelection
-                        onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
-                        disableSelectionOnClick
-                    />
-                </div>
+            <div style={{ display: 'flex', height: '100%', flexGrow: 1 }}>
+                <DataGrid
+                    rows={rows.slice(page * pageSize, page * pageSize + pageSize)}
+                    columns={columns}
+                    pageSize={pageSize}
+                    rowsPerPageOptions={[5, 10, 20, 50, 100]}
+                    onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+                    onSelectionModelChange={(ids) => {
+                        setSelectedColumns(ids)
+                        console.log(ids)
+                    }}
+                    checkboxSelection
+                    disableSelectionOnClick
+                    pagination
+                    components={{
+                        Footer: (paginationProps) => (
+                            <div className="MuiDataGrid-footerContainer" style={{ background: '#676667' }}>
+                                <div>
+                                    <Button variant="contained" color="primary" className={classes.button} onClick={handleRefreshClick}>REFRESH DATA</Button>
+                                    <Button variant="contained" color="primary" className={classes.button} onClick={handleEditClick} disabled={selectedColumns.length !== 1}>EDIT</Button>
+                                    <Button variant="contained" color="primary" className={classes.button} onClick={handleDeleteClick} disabled={selectedColumns.length < 1}>DELETE</Button>
+                                    <Button variant="contained" color="primary" className={classes.button}>PREDICT</Button>
+                                </div>
+                                <div className="MuiTablePagination-root">
+                                    <TablePagination
+                                        {...paginationProps}
+                                        rowsPerPageOptions={[5, 10, 20, 50, 100]}
+                                        count={rows.length}
+                                        rowsPerPage={pageSize}
+                                        page={page}
+                                        onPageChange={(event, newPage) => {
+                                            setPage(newPage);
+                                        }}
+                                        onRowsPerPageChange={(event) => {
+                                            setPageSize(+event.target.value);
+                                        }}
+                                    />
+                                </div>
+                            </div>
+                        ),
+                    }}
+                />
             </div>
+            <Edit open={editOpen} onClose={handleEditClose} onEdit={handleEdit} columnNames={columnNames} disabled={selectedColumns.length !== 1} />
+            <Delete open={deleteOpen} onClose={handleDeleteClose} onDelete={handleDelete} disabled={selectedColumns.length < 1} />
         </div>
     );
 }
+
+
+
--- src/components/modal/Edit.js
+++ src/components/modal/Edit.js
@@ -1,33 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
 
 const useStyles = makeStyles((theme) => ({
+    textField: {
+        marginBottom: theme.spacing(1),
+        height: ""
+    },
     Button: {
-        margin: theme.spacing(1),
-        width: '155px',
+        margin: theme.spacing(2),
+        width: '260px',
     },
 }));
 
-export default function Delete({ open, onClose, onDelete }) {
+export default function Edit({ open, onClose, onEdit, columnNames }) {
     const classes = useStyles();
+    const [formValues, setFormValues] = useState({
+        orderCurrency: '',
+        companyCode: '',
+        distributionChannel: '',
+    });
 
-
-    const handleDelete = () => {
-        onDelete();
+    const handleEdit = () => {
+        onEdit(formValues);
         onClose();
+    };
+
+    const handleInputChange = (event) => {
+        const { id, value } = event.target;
+        setFormValues((prevState) => ({
+            ...prevState,
+            [id]: value,
+        }));
     };
 
     return (
         <Dialog open={open} onClose={onClose}>
-            <DialogTitle>Delete Record?</DialogTitle>
-            <DialogContent>Are you sure you want to delete this record?</DialogContent>
+            <DialogTitle>Edit</DialogTitle>
+            <DialogContent>
+                <form className={classes.root} noValidate autoComplete="off">
+                    <TextField
+                        id="orderCurrency"
+                        label={columnNames.orderCurrency}
+                        variant="outlined"
+                        value={formValues.orderCurrency}
+                        className={classes.textField}
+                        style={{ width: '45%', marginRight: '9px' }}
+                        onChange={handleInputChange} // Add onChange event handler
+                    />
+                    <TextField
+                        id="companyCode"
+                        label={columnNames.companyCode}
+                        variant="outlined"
+                        value={formValues.companyCode}
+                        className={classes.textField}
+                        style={{ width: '49%' }}
+                        onChange={handleInputChange} // Add onChange event handler
+                    />
+                    <TextField
+                        id="distributionChannel"
+                        label={columnNames.distributionChannel}
+                        variant="outlined"
+                        value={formValues.distributionChannel}
+                        className={classes.textField}
+                        style={{ width: '96%' }}
+                        onChange={handleInputChange} // Add onChange event handler
+                    />
+                </form>
+            </DialogContent>
             <DialogActions>
-                <Button variant="contained" onClick={onClose} className={classes.Button}>
-                    Cancel
+                <Button variant="contained" onClick={handleEdit} className={classes.Button}>
+                    EDIT
                 </Button>
-                <Button variant="contained" color="secondary" onClick={handleDelete} className={classes.Button}>
-                    Delete
+                <Button variant="contained" onClick={onClose} className={classes.Button}>
+                    CANCEL
                 </Button>
             </DialogActions>
         </Dialog>
